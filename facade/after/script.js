8cf6010aefe71bb06bcc15d097ca1dc503707943
Matheus Morcinek <matheusmorcinek@gmail.com>
2021-02-11 12:39:44 -0300

    facade example

---
+++ facade/after/script.js
@@ -0,0 +1,32 @@
+const axios = require('axios');
+
+function getUsers() {
+
+    return getFetch('https://jsonplaceholder.typicode.com/users');
+}
+
+function getUserPosts(userId) {
+
+    return getFetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`, { userId: userId });
+}
+
+getUsers().then(users => {
+
+    users.forEach(user => {
+
+        getUserPosts(user.id).then(posts => {
+
+            console.log(user.name);
+            console.log(posts.length);
+        });
+    });
+});
+
+function getFetch(url, params) {
+
+    return axios({
+        url: url,
+        method: 'GET',
+        params: params
+    }).then(response => response.data);
+}
